f05364a7858efb32489ea7f1703d2c57fb71749e
Jordi Sanchez <jordi.sanchez@flywire.com>
2024-06-25 13:20:03 +0200

    Print some stats

--- practice.py
+++ practice.py
@@ -5,6 +5,7 @@ import re
 import sys
 import termios
 import tty
+from time import perf_counter
 from termcolor import colored
 
 
@@ -25,17 +26,23 @@ def read_generator_file(filename):
         return [line.strip() for line in lines if not line.startswith("#")]
 
 
-def start(expressions_filename, words_filename, challenges=25):
+def start(expressions_filename, words_filename, challenges=5):
     generator_expressions = read_generator_file(expressions_filename)
     generator_words = read_generator_file(words_filename)
+
+    valid_strokes = 0
+    invalid_strokes = 0
+    time_between_strokes = 0
+    previous_time = perf_counter()
 
     for i in range(challenges):
         challenge_string = generate_challenge_string(generator_expressions, generator_words)
         text = colored(challenge_string, "light_grey")
         sys.stdout.write(text + "\r")
         sys.stdout.flush()
+        ok_chars = [False] * len(challenge_string)
         pos = 0
-        status = [False] * len(challenge_string)
+
         while True:
             char = getch()
             if char == "\x03":
@@ -46,18 +53,29 @@ def start(expressions_filename, words_filename, challenges=25):
                 sys.stdout.write(f"\b{colored(challenge_string[pos], "light_grey")}\b")
             elif char == challenge_string[pos]:
                 sys.stdout.write(colored(char, "green"))
-                status[pos] = True
+                ok_chars[pos] = True
                 pos += 1
+                valid_strokes += 1
             else:
                 sys.stdout.write(colored(challenge_string[pos], "red"))
                 pos += 1
+                invalid_strokes += 1
             sys.stdout.flush()
+
+            next_time = perf_counter()
+            elapsed_time = next_time - previous_time
+            time_between_strokes += elapsed_time
+            previous_time = next_time
+
             if pos == len(challenge_string):
-                if all(st for st in status):
+                if all(st for st in ok_chars):
                     print(colored(" ✓", "green"))
                 else:
                     print(colored(" x", "red"))
                 break
+    print("# Statistics")
+    print(" Time between strokes: %.2f" % (time_between_strokes / (valid_strokes + invalid_strokes)))
+    print(" Accuracy: %.2f %%" % ((float(valid_strokes) * 100) / (valid_strokes - invalid_strokes)))
 
 
 def generate_challenge_string(generator_expressions, generator_words):
